80f37e17cdccb10924c2c3689c79588bd48d5497
Sofia <sofiamironeko@gmail.com>
2022-12-16 12:12:34 +1100

    more stylin

--- src/form-validation.js
+++ src/form-validation.js
@@ -1,11 +1,6 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
   'use strict'
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
   const forms = document.querySelectorAll('.needs-validation')
-
-  // Loop over them and prevent submission
   Array.prototype.slice.call(forms).forEach(function (form) {
     form.addEventListener(
       'submit',
@@ -14,7 +9,6 @@
           event.preventDefault()
           event.stopPropagation()
         }
-
         form.classList.add('was-validated')
       },
       false
